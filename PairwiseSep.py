50cf5c8b868444ccbdaf5a53fd108e8076be984e
Stepan Petrosian <stephenpetrosian@gmail.com>
2020-03-04 20:46:54 +0300

    Patch error in formula

--- PairwiseSep.py
+++ PairwiseSep.py
@@ -2,9 +2,8 @@ from Bio.PDB import *
 from COM_helix import COM_helix
 import numpy as np
 
-hel_COMs = COM_helix('/home/stephen/Desktop/3b0t.cif')
-
-def PairwiseSep(h1, h2):
-    vect = np.sqrt((np.array(hel_COMs[h1-1]) - np.array(hel_COMs[h2-1]))**2)
+def PairwiseSep(h1, h2, str_file):
+    hel_COMs = COM_helix(str_file)
+    vect = np.array(hel_COMs[h1-1]) - np.array(hel_COMs[h2-1])
     length = np.sqrt(np.sum(vect**2))
     return length

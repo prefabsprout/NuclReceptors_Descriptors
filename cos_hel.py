0883d4bd629b9dba7b2a18a75175f04cf25652ed
Darkwoods <54941976+Darkwoods@users.noreply.github.com>
2020-05-11 22:41:56 +0300

    Add cos_hel.py

---
+++ cos_hel.py
@@ -0,0 +1,35 @@
+from Bio.PDB import *
+import pandas as pd
+
+
+def cos_hel(pdb_file):
+
+    p = PDBParser()
+    structure = p.get_structure('protein', pdb_file)
+    model = structure[0]
+    dssp = DSSP(model, pdb_file)
+
+    helix_borders = []
+
+    for i in range(0, len(dssp.keys())):
+        if dssp[list(dssp.keys())[i]][2] == 'H' and dssp[list(dssp.keys())[i - 1]][2] != 'H':
+            border = [dssp.keys()[i][1][1]]
+        elif dssp[list(dssp.keys())[i]][2] == 'H' and dssp[list(dssp.keys())[i + 1]][2] != 'H':
+            border.append(dssp.keys()[i][1][1])
+            helix_borders.append(border)
+
+    helices = dict()
+    for i in range(0, len(helix_borders)):
+        helices[i] = [structure[0]['A'][res]['CA'].get_vector() for res in [helix_borders[i]][0]]
+
+    for elem in helices:
+        helices[elem] = helices[elem][0]-helices[elem][1]
+
+    cos_between_hel = dict()
+    for el in helices:
+        cos_between_hel[el] = [(helices[el]*helices[an_el]) / \
+                                (((helices[el][0]**2+helices[el][1]**2+helices[el][2]**2)**0.5) *
+                                ((helices[an_el][0]**2+helices[an_el][1]**2+helices[an_el][2]**2)**0.5))
+                                for an_el in helices]
+    data = pd.DataFrame(cos_between_hel).to_csv(pdb_file+'.csv')
+    return data
